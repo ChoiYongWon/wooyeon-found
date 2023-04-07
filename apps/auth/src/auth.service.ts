d966250864d29c95b953c29bb6e85f91f4564ace
ChoiYongWon <yongwon0824@naver.com>
2023-04-08 01:41:50 +0900

    git-action env issue test 2

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   getHello(): string {
-    return 'Hello World Auth Service Ver. 11';
+    return 'Hello World Auth Service Ver. 12';
   }
 }
