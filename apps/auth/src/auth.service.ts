2c6e6c2be38b34e9ed54b933d95328173dad1d8d
ChoiYongWon <yongwon0824@naver.com>
2023-04-12 04:50:04 +0900

    fixed hello message

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   getHello(): string {
-    return 'Hello World Auth Service Ver. 14';
+    return 'Auth Service';
   }
 }
