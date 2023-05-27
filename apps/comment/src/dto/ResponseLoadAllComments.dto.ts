bf54b64b8bcab6e22109ad1dc38977b6b34d2517
ChoiYongWon <yongwon0824@naver.com>
2023-05-28 05:04:51 +0900

    import 에러 수정

--- apps/comment/src/dto/ResponseLoadAllComments.dto.ts
+++ apps/comment/src/dto/ResponseLoadAllComments.dto.ts
@@ -1,6 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { CATEGORY } from '../entity/Categories';
-import { ROLE } from '../entity/Roles';
 
 export class ResponseLoadAllCommentsDto {
   @ApiProperty()
