c8adb9eaa7e64c1d10dc96ea7e2f1e0163ac44d6
ChoiYongWon <yongwon0824@naver.com>
2023-05-27 20:29:56 +0900

    CORS 및 Filter 추가

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from 'apps/post/src/filter/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('우연한 발견 API')
     .setDescription('우연한 발견 API 문서')
