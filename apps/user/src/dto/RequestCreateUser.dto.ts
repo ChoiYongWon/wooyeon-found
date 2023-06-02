1bb73cec6d84aca2f22be0b551390b5f7ec6220e
ChoiYongWon <yongwon0824@naver.com>
2023-06-02 16:34:50 +0900

    user dto 인식 에러 수정

--- apps/user/src/dto/RequestCreateUser.dto.ts
+++ apps/user/src/dto/RequestCreateUser.dto.ts
@@ -1,18 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
+import { IsArray, IsEmail, IsEnum, IsString } from 'class-validator';
 import { ROLE } from '../entity/Roles';
 import { CATEGORY } from '@app/common/enums/category.enum';
 
 export class RequestCreateUserDto {
   @ApiProperty()
+  @IsString()
   name: string;
 
   @ApiProperty()
+  @IsString()
   email: string;
 
   @ApiProperty()
-  message?: string;
-
-  @ApiProperty()
+  @IsEnum(CATEGORY, { each: true })
+  @IsArray()
   category: CATEGORY[];
 }
