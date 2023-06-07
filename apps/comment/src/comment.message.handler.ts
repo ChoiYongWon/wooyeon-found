7fde4185258ad646cc535d02d7aa96f4ba4b2ffa
ChoiYongWon <yongwon0824@naver.com>
2023-06-07 21:41:41 +0900

    comment 이벤트 수신 수정

--- apps/comment/src/comment.message.handler.ts
+++ apps/comment/src/comment.message.handler.ts
@@ -11,9 +11,9 @@ export class MessageHandler {
     /** batch: */ false,
   )
   public async handleUserDeletedMessage(message: Message) {
-    const user_id = JSON.parse(JSON.parse(message.Body).Message);
-    await this.commentService.deleteCommentByUserId(user_id);
-    Logger.log(`comment-user_deleted.fifo ${user_id}`);
+    const data = JSON.parse(JSON.parse(message.Body).Message);
+    await this.commentService.deleteCommentByUserId(data.target_id);
+    Logger.log(`comment-user_deleted.fifo ${data.target_id}`);
   }
 
   @SqsMessageHandler(
@@ -21,8 +21,8 @@ export class MessageHandler {
     /** batch: */ false,
   )
   public async handlePostDeletedMessage(message: Message) {
-    const post_id = JSON.parse(JSON.parse(message.Body).Message);
-    await this.commentService.deleteCommentByPostId(post_id);
-    Logger.log(`comment-post_deleted.fifo ${post_id}`);
+    const data = JSON.parse(JSON.parse(message.Body).Message);
+    await this.commentService.deleteCommentByPostId(data.target_id);
+    Logger.log(`comment-post_deleted.fifo ${data.target_id}`);
   }
 }
